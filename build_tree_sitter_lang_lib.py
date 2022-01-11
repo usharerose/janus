1cde7f1f4ca5573c4060269af505934fd16d9f4a
Chaojie Yan <cyan@appannie.com>
2022-01-12 00:21:59 +0800

    add basic logic of complexity calculation with single file

---
+++ build_tree_sitter_lang_lib.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+
+from tree_sitter import Language
+
+
+TREE_SITTER_LANG_SRC_DIR_ROOT = 'vendor'
+
+
+def build_library():
+    Language.build_library(
+        # Store the library in the `build` directory
+        'build-tree-sitter/my-languages.so',
+
+        # Include one or more languages
+        [os.path.join(TREE_SITTER_LANG_SRC_DIR_ROOT, dir_name)
+         for dir_name in os.listdir(TREE_SITTER_LANG_SRC_DIR_ROOT)]
+    )
+
+
+if __name__ == '__main__':
+    build_library()
