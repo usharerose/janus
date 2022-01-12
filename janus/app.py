055bebce5a20c9386232572980cc1eaa9a0c0ac9
Chaojie Yan <cyan@appannie.com>
2022-01-12 11:00:36 +0800

    update the values of decision node types

--- janus/app.py
+++ janus/app.py
@@ -10,14 +10,13 @@ PY_LANGUAGE = Language('build-tree-sitter/my-languages.so', 'python')
 
 class Janus(object):
 
-    DECISION_TYPE = ['if_statement',
-                     'elif_clause',
-                     'for_statement',
-                     'while_statement',
-                     'except_clause',
-                     'with_statement',
-                     'assert_statement',
-                     'list_comprehension']
+    # refer to the value in https://github.com/tree-sitter/tree-sitter-python/blob/master/src/grammar.json
+    DECISION_TYPES = [
+        'if', 'elif', 'assert',  # condition
+        'for', 'while',          # loop
+        'except', 'with',        # try / except
+        'and', 'or'              # logical operator
+    ]
 
     def __init__(self):
         self.parser = Parser()
@@ -26,18 +25,17 @@ class Janus(object):
     def process(self, f_path):  # NOQA
         with open(f_path, mode='rb') as f:
             tree = self.parser.parse(f.read())
-        complexity = self._search_statement(tree.root_node)
+        complexity = self._search_decisions(tree.root_node)
         return {'data': [{'file_path': f_path,
                           'cyclomatic_complexity': complexity}]}
 
-    def _search_statement(self, node):
+    def _search_decisions(self, node):
         counter = 0
         queue = [node]
         while queue:
             _node = queue.pop(0)
-            if _node.type in self.DECISION_TYPE:
+            if _node.type in self.DECISION_TYPES:
                 counter += 1
-            for _child_node in _node.children:
-                queue.append(_child_node)
+            queue.extend(_node.children)
 
         return counter + 1  # V(G) = P + 1
